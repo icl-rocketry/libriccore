d7880b98a7158afefdc6d8dd9ff6b0e1e838277c
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-12-30 23:35:06 +0000

    Added store

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <fstream>
+#include <utility>
+#include <memory>
 #include <headers/thread.h>
+#include "store/store_base.h"
+#include <exception>
 
 int fib(int n, int a, int b) {
     if (n == 0) {
@@ -19,13 +24,95 @@ void thread2(void* args) {
     l->release(); 
 }
 
+// class UnixStore;
+
+class UnixWrappedFile : public WrappedFile {
+public:
+    UnixWrappedFile(std::string path, FILE_MODE mode, StoreBase& store) : WrappedFile(mode, store) {
+        auto f_mode = std::fstream::in;
+        switch (mode) {
+        case FILE_MODE::READ:
+            break;
+        case FILE_MODE::WRITE:
+            f_mode = std::fstream::out;
+            break;
+        case FILE_MODE::RW:
+            f_mode |= std::fstream::out;
+            break;
+        };
+        file = std::fstream(path, f_mode);
+    }
+
+    void read(char* dest, size_t size) {
+        if (mode == FILE_MODE::WRITE) {
+            throw std::runtime_error("Cannot read from a writeonly file");
+        }
+        file.read(dest, size);
+    }
+    
+    void close() {
+        file.close();
+    }
+
+private:
+    std::fstream file;
+
+    virtual void file_write(const char* data, size_t size) {
+        file.write(data, size);
+    }
+    virtual void file_flush() {
+        file.flush();
+    }
+};
+
+class UnixStore : public StoreBase {
+public:
+    UnixStore(Lock& device_lock) : StoreBase(device_lock) {}
+
+    std::unique_ptr<WrappedFile> open(std::string path, FILE_MODE mode) {
+        auto file = std::make_unique<UnixWrappedFile>(path, mode, *this);
+        intptr_t idx = (intptr_t) (file.get());
+        queues.emplace(std::piecewise_construct,
+                       std::forward_as_tuple(idx),
+                       std::forward_as_tuple());
+        return file;
+    }
+
+    bool ls(std::string path, std::vector<directory_element_t> &directory_structure) {
+        return true;
+    }
+    bool mkdir(std::string path) {
+        return true;
+    }
+
+    // Removes a file or an empty directory
+    bool remove(std::string path) {
+        return true;
+    }
+};
+
+
 int main() {
     Lock l;
-    l.acquire();
+    // l.acquire();
 
-    Thread t(&thread2, (void*) &l);
+    // Thread t(&thread2, (void*) &l);
 
-    l.acquire(); // Wait for thread 2 to complete
+    // l.acquire(); // Wait for thread 2 to complete
+
+    UnixStore s(l);
+    
+    auto file = s.open("./tmp/asdf.txt", FILE_MODE::WRITE);
+
+    const char* str = "hello world\n";
+    bool done;
+
+    file->append(str, 12, &done);
+
+    while (!done) {
+        std::cout << "waiting" << std::endl;
+    }
+
 
     std::cout << "Done" << std::endl;
     return 0;
--- src/store/store_base.h
+++ src/store/store_base.h
@@ -3,8 +3,10 @@
 #include <string>
 #include <vector>
 #include <cstdint>
+#include <memory>
 #include <unordered_map>
-#include <thread.h>
+
+#include <headers/thread.h>
 
 
 enum class FILE_TYPE : uint8_t {
@@ -24,92 +26,60 @@ struct directory_element_t{
     FILE_TYPE type;
 };
 
-class StoreBase;
-
-struct AppendRequest;
-
-class WrappedFile {
-public:
-    WrappedFile(FILE_MODE mode, StoreBase& store) : cursor(0), store(store), mode(mode) {}
-
-    // Send a request to the underlying store to store this
-    void append(const char* data, size_t size, bool* done) {
-        AppendRequest req{data, done, size};
-        store.append(*this,  req);
-    }
-
-    virtual void read(char* dest, size_t size);
-    virtual void close(); // Should this be done automatically on delete?
-
-private:
-    size_t cursor;
-    StoreBase& store;
-    FILE_MODE mode;
-
-    //Write/Flush to the underlying file type
-    virtual void file_write(const char* data, size_t size);
-    virtual void file_flush();
-
-friend StoreBase;
-};
-
-
 struct AppendRequest {
     const char* data;
     bool* done;
     size_t size;
 };
 
+class StoreBase;
+class WrappedFile {
+public:
+    WrappedFile(FILE_MODE mode, StoreBase& store) : mode(mode), store(store) {}
+
+    // Send a request to the underlying store to store this
+    void append(const char* data, size_t size, bool* done);
+
+    virtual void read(char* dest, size_t size) = 0;
+    virtual void close() = 0; // Should this be done automatically on delete?
+
+protected:
+    FILE_MODE mode;
+
+private:
+    StoreBase& store;
+
+    //Write/Flush to the underlying file type
+    virtual void file_write(const char* data, size_t size) = 0;
+    virtual void file_flush() = 0;
+
+friend StoreBase;
+};
+
+
 class StoreBase {
 public:
-    StoreBase(std::string name, Lock& device_lock) : 
-    t((void (*)(void* args)) &StoreBase::flush_task, nullptr), device_lock(device_lock), queues({}) {}
+    StoreBase(Lock& device_lock) : device_lock(device_lock), t((void (*)(void* args)) &StoreBase::flush_task, (void*) this) {}
 
     bool ls(std::vector<directory_element_t> &directory_structure) {
         return ls("/", directory_structure);
     }
 
-    void append(WrappedFile& file, AppendRequest r) {
-        queues.at(file).send(r);
-        remaining_work.up(); //Must have sequential consistency (guarantee that the up happens after the channel send)
-    }
+    void append(WrappedFile& file, AppendRequest r);
 
-    virtual WrappedFile open(std::string path, FILE_MODE mode);
-    virtual bool ls(std::string path, std::vector<directory_element_t> &directory_structure);
-    virtual bool mkdir(std::string path);
-    virtual bool remove(std::string path); // Removes a file or an empty directory
+    virtual std::unique_ptr<WrappedFile> open(std::string path, FILE_MODE mode) = 0;
+    virtual bool ls(std::string path, std::vector<directory_element_t> &directory_structure) = 0;
+    virtual bool mkdir(std::string path) = 0;
+    virtual bool remove(std::string path) = 0; // Removes a file or an empty directory
 
     Lock& device_lock;
 
-private:
-    void flush_task(void* args) {
-        AppendRequest req;
-        while (true) {
-            for (auto& [file, queue] : queues) {
-                bool pending_flush = false;
-                
-                while (!queue.empty()) {
-                    remaining_work.down();
-                    queue.receive(req);
-                    
-                    {
-                        ScopedLock l(device_lock);
-                        file.file_write(req.data, req.size);
-                    }
-                    if (req.done != nullptr) {
-                        *req.done = true;
-                    }
-                    pending_flush = true;
-                }
+    void flush_task(void*);
+protected:
+    std::unordered_map<intptr_t, Channel<AppendRequest>> queues;
 
-                if (pending_flush) {
-                    file.file_flush();
-                }
-            }
-        }
-    }
+private:
 
     Thread t;
     Semaphore remaining_work;
-    std::unordered_map<WrappedFile&, Channel<AppendRequest>> queues;
 };
