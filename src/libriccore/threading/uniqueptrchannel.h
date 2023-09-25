42963e2cea20c83804a41edca9af5fa7e0b5159c
kiran <kd619@ic.ac.uk>
2023-09-25 20:48:14 +0100

    fixed potential race condition where flush task would erase queue while store base is appending

--- src/libriccore/threading/uniqueptrchannel.h
+++ src/libriccore/threading/uniqueptrchannel.h
@@ -68,6 +68,12 @@ namespace RicCoreThread
             return vec.empty();
         }
 
+        void clear()
+        {
+            auto l = ScopedLock(lock);
+            vec = {};
+        }
+
         size_t size()
         {
             auto l = ScopedLock(lock);
