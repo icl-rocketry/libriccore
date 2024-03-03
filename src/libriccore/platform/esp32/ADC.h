002615ace0bb6237b890e49756fe89ed0cd3fcb2
Soham <ssm422@ic.ac.uk>
2024-03-03 22:38:40 +0000

    More messing around with the ADC

--- src/libriccore/platform/esp32/ADC.h
+++ src/libriccore/platform/esp32/ADC.h
@@ -1,4 +1,12 @@
 #pragma once
+/**
+ * @file ADC.h
+ * @author Soham More
+ * @brief ADC wrapper for ESP32
+ * @date 2024
+ */
+
+
 #include <string>
 #include <stdio.h>
 #include <stdint.h>
@@ -10,67 +18,70 @@
 
 
 // ADC reference voltage = 1100mV, however, this can range between 1000mV and 1200mV.
-// Calibration values are stored in the eFuse or provided by the user using two point (V1, V2) readings.
 
 class ADC {
 
 private:
+
+    //
+    const std::string _name;
+    static constexpr auto LOG_TARGET = RicCoreLoggingConfig::LOGGERS::SYS;  
     static const adc_unit_t _unit;
     static constexpr adc_atten_t atten = ADC_ATTEN_DB_11;
-    static const adc1_channel_t adc1_channel;
-    static const adc2_channel_t adc2_channel;
+    esp_adc_cal_characteristics_t _adcCal;
     static constexpr adc_bits_width_t width;
-    const uint8_t pin;
+    static constexpr adc_atten_t _atten = ADC_ATTEN_DB_11;
+    const uint8_t _pin;
     bool _adcInitialized = false;
-    
-
-    struct adc_raw{
-        int16_t adc1_raw;
-        int16_t adc2_raw;
-        };
+    static constexpr int VREF = 1100;
+    uint16_t adc1_raw;
+    uint16_t adc2_raw;
 
 public:
-    ADC(ADC_Channel_t ADC_Channel, const uint8_t pin);
+    ADC(std::string_view ADC_name const uint8_t pin):
+    _name(ADC_name),
+    _pin(pin)
+    {}; 
 
-void calibration(){
-    
-    //Check if TP is burned into eFuse
-    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) {
-        RicCoreLogging::log<LOGGING_TARGET>("eFuse Two Point: Supported\n");
-        
-    } else {
-        RicCoreLogging::log<LOGGING_TARGET>("eFuse Two Point: NOT supported\n");
+void ADC::setup(){
+
+    int error = 0;
+    int channel = digitalPinToAnalogChannel(_pin);
+    //Configure ADC channel
+    if (channel > (SOC_ADC_MAX_CHANNEL_NUM - 1)) 
+    {
+        _channel = static_cast<adc_channel_t>(channel-SOC_ADC_MAX_CHANNEL_NUM);
+        _unit = ADC_UNIT_2;
+
+        error += adc2_config_channel_atten(static_cast<adc_channel_t>(_channel), _atten);
+    }
+    else 
+    {
+        _channel = static_cast<adc_channel_t>(channel);
+        _unit = ADC_UNIT_1;
+        error += adc1_config_width(width);
+        error += adc1_config_channel_atten(static_cast<adc_channel_t>(_channel), _atten);
     }
 
-   //Check Vref is burned into eFuse
-    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK) {
-        RicCoreLogging::log<LOGGING_TARGET>("eFuse Vref: Supported\n");
-
-    } else {
-        RicCoreLogging::log<LOGGING_TARGET>("eFuse Vref: NOT supported\n");
+    if (error) 
+    {
+        RicCoreLogging::log<RicCoreLoggingConfig::LOGGERS::SYS>("ADC setup failed");
+        return;
     }
 
-    //Configure ADC
-
-    if (_unit == ADC_UNIT_1) {
-        adc1_config_width(width);
-        adc1_config_channel_atten(adc1_channel, atten);
-    } else {
-        adc2_config_width(width);
-        adc2_config_channel_atten((adc2_channel_t)adc2_channel, atten);
-    }
-
+    esp_adc_cal_characterize(_unit, _atten, width, VREF, &_adcCal);
     _adcInitialized = true;
+    RicCoreLogging::log<RicCoreLoggingConfig::LOGGERS::SYS>("ADC setup complete");
 
 }
 
-void update(){
+void ADC::update(){
 
     if (!_adcInitialized) {
         return;
     }
     
-    if (_unit = ADC_UNIT_1) {
+    if (_unit == ADC_UNIT_1) {
          adc1_raw = adc1_get_raw(adc1_channel_t channel);
     } 
     else {
@@ -78,13 +89,14 @@ void update(){
     }   
 }
 
-int16_t getADC1(){
+int16_t ADC::getADC(adc_unit_t _unit, adc_channel_t channel){
 
-    return adc1_channel;
+    if (_unit == ADC_UNIT_1) {
+        return adc1_raw;
+    } 
+    else {
+        return adc2_raw;
+    }
 }
-
-int16_t getADC2(){
-
-    return adc2_channel;
 
 };
