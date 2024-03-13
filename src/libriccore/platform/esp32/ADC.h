a86258dd23a7266603f6cbb9cfe210d09bb0c55a
Soham <ssm422@ic.ac.uk>
2024-03-13 17:37:55 +0000

    previous commit doesn't work, this does (i think)

--- src/libriccore/platform/esp32/ADC.h
+++ src/libriccore/platform/esp32/ADC.h
@@ -6,84 +6,64 @@
  * @date 2024
  */
 
-
 #include <stdint.h>
 #include <driver/adc.h>
 #include <esp_adc_cal.h>
-
+#include <esp32-hal-gpio.h>
 
 // ADC reference voltage = 1100mV, however, this can range between 1000mV and 1200mV.
 
-class ADC {
+class ADC
+{
 
 public:
+    ADC(const uint8_t pin) : _pin(pin)
+                                {};
 
-    ADC(const uint8_t pin):
-        _pin(pin)
-    {}; 
-
-void setup(){
-
-    esp_err_t error;
-    int channel = digitalPinToAnalogChannel(_pin);
-
-    //Configure ADC channel
-    if (channel > (SOC_ADC_MAX_CHANNEL_NUM - 1)) 
+    void setup()
     {
-        _channel = static_cast<adc_channel_t>(channel-SOC_ADC_MAX_CHANNEL_NUM);
-        _unit = ADC_UNIT_2;
 
-        error += adc2_config_channel_atten(static_cast<adc_channel_t>(_channel), _atten);
+        // Configure ADC channel
+
+        int8_t channel = digitalPinToAnalogChannel(_pin);
+        _channel = static_cast<adc1_channel_t>(channel);
+
+        adc1_config_width(width);
+        adc1_config_channel_atten(_channel, _atten);
+
+        esp_adc_cal_characterize(_unit, _atten, width, VREF, &_adcCal);
+        _adcInitialized = true;
     }
-    else 
+
+    void update()
     {
-        _channel = static_cast<adc_channel_t>(channel);
-        _unit = ADC_UNIT_1;
-        error += adc1_config_width(width);
-        error += adc1_config_channel_atten(static_cast<adc_channel_t>(_channel), _atten);
+
+        if (!_adcInitialized)
+        {
+            return;
+        }
+
+        adc1_raw = adc1_get_raw(_channel);
     }
 
-    esp_adc_cal_characterize(_unit, _atten, width, VREF, &_adcCal);
-    _adcInitialized = true;
+    int16_t getADC()
+    {
 
-}
-
-void update(){
-
-    if (!_adcInitialized) {
-        return;
-    }
-    
-    if (_unit == ADC_UNIT_1) {
-         adc1_raw = adc1_get_raw(adc1_channel_t channel);
-    } 
-    else {
-    adc2_raw = adc2_get_raw(adc2_channel_t channel, adc_bits_width_t width, &adc2_raw);
-    }   
-}
-
-int16_t getADC(adc_unit_t _unit, adc_channel_t channel){
-
-    if (_unit == 1) {
         return adc1_raw;
-    } 
-    else {
-        return adc2_raw;
     }
-}
+
+    const uint8_t _pin;
+    adc_channel_t channel;
+    adc1_channel_t _channel;
 
 private:
-  
-    static const adc_unit_t _unit;
+    adc_unit_t _unit = ADC_UNIT_1;
     static constexpr adc_atten_t atten = ADC_ATTEN_DB_11;
     esp_adc_cal_characteristics_t _adcCal;
     static constexpr adc_bits_width_t width = ADC_WIDTH_BIT_12;
     static constexpr adc_atten_t _atten = ADC_ATTEN_DB_11;
-    const uint8_t _pin;
     bool _adcInitialized = false;
     static constexpr int VREF = 1100;
     uint16_t adc1_raw;
     uint16_t adc2_raw;
-
-
 };
