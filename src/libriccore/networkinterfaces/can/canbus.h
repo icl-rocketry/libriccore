36dc9e0bfb92d8bed2a1b0efdfd58e91d5f37e27
kiran <kd619@ic.ac.uk>
2024-10-11 01:30:32 +0100

    increased can bus size

--- src/libriccore/networkinterfaces/can/canbus.h
+++ src/libriccore/networkinterfaces/can/canbus.h
@@ -62,8 +62,8 @@ public:
                                                                                                                                           can_filter_config(TWAI_FILTER_CONFIG_ACCEPT_ALL())
     {
         _info.MTU = 256;                  // theoretical maximum is 2048 but this is very chonky
-        _info.maxSendBufferElements = 10; // maximum of 10 buffered rnp packets equating to a potential maximum of 2.56kb of buffer storage + sizeof(rnpcanidentifer)*10
-        _info.maxReceiveBufferElements = 10;
+        _info.maxSendBufferElements = 20; // maximum of 10 buffered rnp packets equating to a potential maximum of 2.56kb of buffer storage + sizeof(rnpcanidentifer)*10
+        _info.maxReceiveBufferElements = 20;
     };
 
     void setup() override
