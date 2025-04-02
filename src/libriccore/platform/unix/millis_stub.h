da2ada6e2794334c620faabf95994b4fcea008ec
artemsheyk <artem.a.sheykin@outlook.com>
2025-04-02 14:57:50 +0100

    global variable name change in millis_stub

--- src/libriccore/platform/unix/millis_stub.h
+++ src/libriccore/platform/unix/millis_stub.h
@@ -1,16 +1,16 @@
 #pragma once
 #include <chrono>
 
-inline thread_local auto programStartTime = std::chrono::steady_clock::now();
+inline thread_local auto threadStartTime = std::chrono::steady_clock::now();
 inline thread_local double clockDriftMultiplier = 1.0;
 
 inline void setClockDriftPPM(int ppm) {
     clockDriftMultiplier = 1.0 + static_cast<double>(ppm) / 1e6;
-	programStartTime = std::chrono::steady_clock::now();
+	threadStartTime = std::chrono::steady_clock::now();
 }
 
 inline uint32_t millis() {
-    auto elapsed = std::chrono::steady_clock::now() - programStartTime;
+    auto elapsed = std::chrono::steady_clock::now() - threadStartTime;
     auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count();
     return static_cast<uint32_t>(static_cast<double>(ms) * clockDriftMultiplier);
 }
